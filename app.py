ef43854bf56bcf923ffae9cd2846bbc7620597c8
matthieulebret <73009187+matthieulebret@users.noreply.github.com>
2020-11-05 10:18:11 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -49,6 +49,10 @@ st.header('Word cloud from Wikipedia summary')
 wiki = st.text_input(label='Search Wikipedia',value = 'LinkedIn')
 
 if wiki is not None:
-    sc.gen_stylecloud(text=wikipedia.summary(wiki),output_name='wiki.png',background_color='black')
-    st.subheader('The word cloud from the Wiki search is:')
-    st.image('wiki.png')
+    col1,col2 = st.beta_columns(2,1,2)
+    with col1:
+        sc.gen_stylecloud(text=wikipedia.summary(wiki),output_name='wiki.png',background_color='black')
+        st.subheader('The word cloud from the Wiki search is:')
+        st.image('wiki.png')
+    with col2:
+        st.write(wiki)
