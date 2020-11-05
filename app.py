9ff22615d5a09e6070eccf060d9317295e77e264
matthieulebret <73009187+matthieulebret@users.noreply.github.com>
2020-11-05 16:04:14 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -16,6 +16,15 @@ from requests import get
 import streamlit as st
 
 import stylecloud as sc
+import nltk
+import textblob
+
+from textblob import TextBlob
+
+from nltk.corpus import stopwords
+
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
 
 st.set_page_config('Word cloud app',layout='wide')
 
@@ -32,25 +41,59 @@ with col3:
     st.image('https://images.unsplash.com/photo-1457369804613-52c61a468e7d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',use_column_width=True)
 
 
-st.header('Word cloud from text input')
+st.header('Word cloud from text')
 
-text = st.text_area(label = 'Text input area',value='Input text here',height=300)
+stop_words = stopwords.words('english')
+col1,col2 = st.beta_columns(2)
 
-if text is not None:
-    output = sc.gen_stylecloud(text,output_name='output.png',background_color='black',icon_name = 'fas fa-brain')
-    st.subheader('The word cloud from the text is:')
-    st.image('output.png')
-else:
-    pass
+with col1:
+    st.subheader('Input text')
+    text = st.text_area(label = ' ',value='This is really a great wonderful place',height=435)
+with col2:
+    if text is not None:
+        output = sc.gen_stylecloud(text,output_name='output.png',background_color='black',icon_name = 'fas fa-brain',stopwords=stop_words)
+        st.subheader('The word cloud from the text is:')
+        st.image('output.png',use_column_width=True)
+    else:
+        pass
+
+st.header('Sentiment analysis')
+phrase = TextBlob(text)
+
+polarity = phrase.sentiment.polarity
+subjectivity = phrase.sentiment.subjectivity
+
+st.markdown('The positivity level of the text is '+str('{:.2%}'.format(polarity)))
+st.markdown('The subjectivity level of the text is '+str('{:.2%}'.format(subjectivity)))
+
+df = pd.DataFrame([[polarity,subjectivity,0.5,0]])
+df.columns=['Polarity','Subjectivity','Horizontal','Vertical']
 
 
-st.header('Word cloud from Wikipedia summary')
+highlight = alt.selection(type='interval',bind='scales',encodings=['x','y'])
 
-wiki = st.text_input(label='Search Wikipedia',value = 'LinkedIn')
+line = alt.Chart(df).mark_circle(size=200).encode(alt.X('Polarity:Q',axis=alt.Axis(format='%'),scale=alt.Scale(domain=[-1,1])),alt.Y('Subjectivity:Q',axis=alt.Axis(format='%'),scale=alt.Scale(domain=[0,1])),tooltip=[
+      {"type": "quantitative", "field": "Polarity",'format':',.2%'},
+      {"type": "quantitative", "field": "Subjectivity",'format':',.2%'}]).add_selection(highlight)
 
-if wiki is not None:
-    sc.gen_stylecloud(text=wikipedia.page(wiki).content,output_name='wiki.png',background_color='black')
-    st.subheader('The word cloud from the Wiki search is:')
-    st.image('wiki.png')
+rule1 = alt.Chart(df).mark_rule().encode(alt.Y('Horizontal:Q',title='Subjectivity'),size=alt.value(1),color=alt.value('red'))
+rule2 = alt.Chart(df).mark_rule().encode(alt.X('Vertical:Q',title='Polarity'),size=alt.value(1),color=alt.value('red'))
+
+st.altair_chart(line+rule1+rule2,use_container_width=True)
+
+
+
+st.header('Word cloud from Wikipedia article')
+
+col1,col2 = st.beta_columns(2)
+
+with col1:
+    st.subheader('Wikipedia search')
+    wiki = st.text_input(label=' ',value = 'LinkedIn')
     with st.beta_expander('Show Wikipedia article'):
         st.write(wikipedia.page(wiki).content)
+with col2:
+    if wiki is not None:
+        sc.gen_stylecloud(text=wikipedia.page(wiki).content,output_name='wiki.png',background_color='black',stopwords=stop_words)
+        st.subheader('The word cloud from the Wiki search is:')
+        st.image('wiki.png')
